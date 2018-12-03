bc6ec42767cc6648b5570bbdf758d897c8b39f71
gioboa <giorgiob.boa@gmail.com>
2018-12-03 22:17:03 +0100

    feat: favorite filters implementation

--- src/commands/favourites-filters.ts
+++ src/commands/favourites-filters.ts
@@ -1,4 +1,5 @@
-import * as vscode from 'vscode';
+import { SEARCH_MODE } from '../shared/constants';
+import { selectFavoriteFilters, selectIssue } from '../shared/select-utilities';
 import { printErrorMessageInOutput } from '../state/state';
 import { Command } from './shared/command';
 
@@ -7,7 +8,10 @@ export class FavouritesFiltersCommand implements Command {
 
   public async run(): Promise<void> {
     try {
-      vscode.window.showInformationMessage('FavouritesFiltersCommand');
+      const filter = await selectFavoriteFilters();
+      if (filter) {
+        selectIssue(SEARCH_MODE.FAVOURITES_FILTERS, [filter.name, filter.jql]);
+      }
     } catch (err) {
       printErrorMessageInOutput(err);
     }
