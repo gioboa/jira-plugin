ba4f6c1fa7498c1e71eb15dd5c706c75a658ee97
gioboa <giorgiob.boa@gmail.com>
2019-02-26 00:25:17 +0100

    test: settings file

---
+++ test/utils/settings.ts
@@ -0,0 +1,12 @@
+import { ISettings } from './settings.model';
+
+export const settings: ISettings = {
+  baseUrl: 'xxx',
+  username: 'xxx',
+  password: 'xxx',
+  workingProject: '',
+  enableWorkingIssue: false,
+  workingIssueStatues: 'In progress',
+  counter: 0,
+  workingIssue: undefined
+};
