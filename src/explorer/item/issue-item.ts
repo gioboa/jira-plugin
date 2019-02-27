ed65cddd94c22e60cce656940ce831da3d6d08d0
gioboa <giorgiob.boa@gmail.com>
2019-02-28 00:47:58 +0100

    fix: added subtasks annotation in parent label

--- src/explorer/item/issue-item.ts
+++ src/explorer/item/issue-item.ts
@@ -2,13 +2,14 @@ import * as vscode from 'vscode';
 import { IIssue } from '../../services/http.model';
 
 export class IssueItem extends vscode.TreeItem {
+  private isCollapsible = (issue: IIssue): boolean => (!!issue.fields.subtasks && !!issue.fields.subtasks.length ? true : false);
+
   constructor(public readonly issue: IIssue, public readonly command?: vscode.Command) {
-    super(
-      `${issue.key} - ${issue.fields.summary}`,
-      !!issue.fields.subtasks && !!issue.fields.subtasks.length
-        ? vscode.TreeItemCollapsibleState.Collapsed
-        : vscode.TreeItemCollapsibleState.None
-    );
+    super(`${issue.key} - ${issue.fields.summary}`, vscode.TreeItemCollapsibleState.None);
+    if (this.isCollapsible(issue)) {
+      this.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+      this.label += ' - subtasks: ' + (issue.fields.subtasks || []).map(issue => issue.key).join(', ');
+    }
   }
 
   get tooltip(): string {
