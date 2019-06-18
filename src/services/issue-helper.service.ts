c4b6ee8c166bb0207eb20a63977321837489f30c
gioboa <giorgiob.boa@gmail.com>
2019-06-19 01:32:21 +0200

    feat: added new ticket key in creation dialog
    Close: #112

--- src/services/issue-helper.service.ts
+++ src/services/issue-helper.service.ts
@@ -306,7 +306,7 @@ export default class IssueHelperService {
     if (!!createdIssue && !!createdIssue.key) {
       // if the response is ok, we will open the created issue
       vscode.commands.executeCommand('jira-plugin.refresh');
-      const action = await vscode.window.showInformationMessage('Issue created', 'Open in browser', 'Set as working issue');
+      const action = await vscode.window.showInformationMessage(`Issue created: ${createdIssue.key}`, 'Open in browser', 'Set as working issue');
       if (action === 'Open in browser') {
         openIssue(createdIssue.key);
       }
