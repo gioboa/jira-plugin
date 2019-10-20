db9febb91b21a5db5d163ff540ed8e418f9e03d9
gioboa <giorgiob.boa@gmail.com>
2019-10-21 00:12:30 +0200

    fix: removed obsolete method

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -65,10 +65,6 @@ export class Jira implements IJira {
     });
 
     patchJiraInstance(this.jiraInstance);
-
-    if (!!this.jiraInstance.testConnection) {
-      this.jiraInstance.testConnection();
-    }
   }
 
   async getCloudSession(): Promise<{ name: string; value: string }> {
